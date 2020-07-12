d3f35eeb7a9a291ba49ed60a3a7aad0d918ef2a1
Krishpanchal <krishpanchal45@gmail.com>
2020-07-12 13:45:06 +0530

    bcrypt auth

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const express=require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const mongoose=require("mongoose");
-const encrypt=require("mongoose-encryption");
+// const encrypt=require("mongoose-encryption");
 const app=express();
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 
 app.use(express.static("public"));
 app.set('view engine','ejs');
@@ -20,7 +23,7 @@ const userSchema=new mongoose.Schema({
 });
 
 //whenever we call save method it will encrypt and whenever we call fins it will decrypt
-userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:['password']});
+// userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:['password']});
 
 const User = new mongoose.model("User",userSchema);
 
@@ -40,30 +43,36 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
   const email=req.body.username;
   const password =req.body.password;
-  const user=new User({
-    email:email,
-    password:password
-  });
-  user.save(function(err){
-    if(!err){
-      res.render("secrets");
-    }
-  });
+  bcrypt.hash(password, saltRounds, function(err, hash) {
+    const user=new User({
+      email:email,
+      password:hash
+    });
+    user.save(function(err){
+      if(!err){
+        res.render("secrets");
+      }
+    });
+});
+
 });
 
 app.post("/login",function(req,res){
   const username=req.body.username;
   const password=req.body.password;
   User.findOne({email:username},function(err,user){
-    if(!err){
-      if(password===user.password){
-        res.render("secrets");
-      }
-      else{
-        console.log("invalid user");
+      if(!err){
+        bcrypt.compare(password, user.password, function(err, result) {
+        if(result == true){
+          res.render("secrets");
+          }
+        else{
+          console.log("invalid user");
+          }
+        });
       }
     }
-  })
+  )
 })
 app.listen(3000,function(){
   console.log("server started succesfully");
